04d52f29f90758f76e0966769834ad771b4f9099
Abhisheksingh10022 <84452461+Abhisheksingh10022@users.noreply.github.com>
2021-12-06 08:21:14 +0530

    add restaurant

--- client/src/RestaurantCard.jsx
+++ client/src/RestaurantCard.jsx
@@ -1,32 +1,51 @@
 import React from "react";
 import { AiFillStar} from "react-icons/ai";
-import {BiRupee} from "react-icons/bi";
 
-const RestaurantCard=()=>{
+
+const RestaurantCard=(props)=>{
     return (
         <>
-        <div className=" flex flex-col gap-2">
-            <div className="text-xl font-semibold ">Order from your favourite restaurant</div>
-            <div>
-        <div className="bg-white p-2 rounded-lg  sm:w-1/2 lg:w-1/3 flex flex-col gap-3 s hover:shadow ">
-            <div className=" w-full h-56">
-                <img src="https://b.zmtcdn.com/data/pictures/0/19696700/df199718c83d159a4bed17c42d68b3fc_o2_featured_v2.jpg?output-format=webp"
-                alt=""
-                className="w-full h-full rounded-lg" />
+        <div className="bg-white p-4 mb-4  w-full rounded-2xl transition duration-700 ease-in-out hover:shadow-lg md:w-1/2 lg:w-1/3">
+        <div className="w-full h-56 lg:h-64 relative">
+          <div className="absolute w-full bottom-4 flex items-end justify-between">
+            <div className="flex flex-col gap-2 items-start">
+              {props.isPro && (
+                <span className="bg-zomato-400 text-white px-2 py-1 rounded text-sm">
+                  Pro extra 10% off
+                </span>
+              )}
+              {props.isOff && (
+                <span className="bg-blue-600 text-white px-2 py-1 rounded text-sm">
+                  ₹{`${props.isOff}`} OFF
+                </span>
+              )}
             </div>
-             <div className="flex flex-col gap-2 ">
-                 <div className="flex justify-between">
-                 <h4 className=" font-medium text-xl">  Jani Sweets and Snacks </h4>
-                 <span className="bg-black flex items-center text-white">4.1<AiFillStar /></span>
-                 </div>
-                 <div className="flex justify-between">
-                 <p className="text-sm text-gray-500" >Mithai,Street Food,North Indian</p>
-                 <span className="flex items-center"><BiRupee />350</span>
-                 </div>
-             </div>
+            <span className="bg-white bg-opacity-75 p-1 rounded	mr-3">
+              {props.durationOfdelivery} min
+            </span>
+          </div>
+          <img
+            src={props.photos.length&&props.photos[0]}
+            alt="food"
+            className="w-full h-full rounded-2xl"
+          />
         </div>
+        <div className="my-2 flex flex-col gap-2">
+          <div className="flex items-center justify-between">
+            <h4 className="text-xl font-medium">{props.name}</h4>
+            <span className="bg-green-800 text-white text-sm p-1 rounded flex items-center">
+            {props.restaurantReviewvalue}   <AiFillStar />
+            </span>
+          </div>
+          <div className="flex items-center justify-between text-gray-500">
+            <p>{props.cuisine.join(", ")}</p>
+            <p>₹ {props.averageCost} for one</p>
+          </div>
         </div>
-        </div>
+      </div>
+
+
+        
         </>
     )
 }
--- client/src/components/Delivery/index.jsx
+++ client/src/components/Delivery/index.jsx
@@ -1,13 +1,104 @@
-import React from "react";
+import React,{useState} from "react";
 import DeliveryCarousel from "./DeliveryCarousel";
 import Brand from "./Brand";
 import RestaurantCard from "../../RestaurantCard";
 const Delivery=()=>{
+    const [RestaurantList,setRestaurnatlist]=useState([
+        {
+     _id:"123456",
+     photos:[
+         "https://b.zmtcdn.com/data/pictures/chains/7/19073197/7dfd32c0d1cde6e41c32be07b3153c0a.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+     ],
+     name:"SK Amritsari Kulcha Hut",
+     cuisine:["North Indian","Beverages"],
+     averageCost:100,
+     isPro:true,
+     isOff:80,
+     durationOfdelivery:47,
+     restaurantReviewvalue:3.9
+        },
+        {
+            _id:"123456-2",
+     photos:[
+         "https://b.zmtcdn.com/data/pictures/7/18848357/007b6a6e20cf08720fb85f68e5446516.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+     ],
+     name:"Pradhan Snacks",
+     cuisine:["North Indian"],
+     averageCost:100,
+     isPro:false,
+     isOff:0,
+     durationOfdelivery:47,
+     restaurantReviewvalue:3.7
+        },
+        {
+            _id:"123456-3",
+            photos:[
+                "https://b.zmtcdn.com/data/pictures/5/18800025/4ad9cc9cb2bc33b16f7478d9ec2c1539.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+            ],
+            name:"Pradhan Snacks",
+            cuisine:["North Indian","chinese","South indian","Fast Food"],
+            averageCost:100,
+            isPro:true,
+            isOff:10,
+            durationOfdelivery:47,
+            restaurantReviewvalue:3.3
+        },
+        {
+            _id:"123456-4",
+            photos:[
+                "https://b.zmtcdn.com/data/pictures/chains/7/18825237/4004a47fb2f7c02bfa70a529b1514030.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+            ],
+            name:"Salsa Fast Food",
+            cuisine:["North Indian","chinese","South indian","Fast Food"],
+            averageCost:100,
+            isPro:true,
+            isOff:30,
+            durationOfdelivery:47,
+            restaurantReviewvalue:3.3
+        },
+        {
+            _id:"123456-5",
+            photos:[
+                "https://b.zmtcdn.com/data/pictures/0/18945320/27fd0a080d5f67307b6ba48efdd60546_o2_featured_v2.jpg?output-format=webp"
+            ],
+            name:"The Grand Hill",
+            cuisine:["North Indian","chinese","Kashmiri","Fast Food"],
+            averageCost:150,
+            isPro:true,
+            isOff:30,
+            durationOfdelivery:56,
+            restaurantReviewvalue:3.3
+        }
+        ,{
+            _id:"123456-6",
+            photos:[
+                "https://b.zmtcdn.com/data/pictures/chains/0/19041480/6c1d8f2952f7369ba14aa4014ee2693a_o2_featured_v2.jpg?output-format=webp"
+            ],
+            name:"Kaka Di Rasoi",
+            cuisine:["North Indian","chinese"],
+            averageCost:150,
+            isPro:true,
+            isOff:30,
+            durationOfdelivery:58,
+            restaurantReviewvalue:3.7 
+        }
+
+    ]);
     return(
         <>
         <DeliveryCarousel />
         <Brand />
-        <RestaurantCard />
+        <h3 className="text-xl  font-semibold">Pallanwala Restaurants,Jammu</h3>
+        <div className="flex justify-between flex-wrap">
+      {RestaurantList.map((restaurant)=>{
+          return(
+       <RestaurantCard {...restaurant} key={restaurant._id} />
+          )
+      })
+
+       }
+        </div>
+      
         </>
     )
 }
